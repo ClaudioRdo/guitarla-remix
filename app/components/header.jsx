51cb6994e80dbcdfd296536a89ddb100e2859259
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-12-26 13:20:10 -0300

    Added logo

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,10 +1,13 @@
 import { Link } from '@remix-run/react';
+import logo from '../../public/img/logo.svg'
 
 const Header = () => {
     return (
         <header className="header">
             <div className="cantainer bar">
-                <div className="logo"></div>
+                <Link to="/">
+                    <img src={logo} alt="logo image" className='logo'/>
+                </Link>
                 <nav className="navigation">
                     <Link
                         to="/"
@@ -23,7 +26,6 @@ const Header = () => {
                         prefetch='render'
                     >Blog</Link>
                 </nav>
-
             </div>
         </header>
     )
